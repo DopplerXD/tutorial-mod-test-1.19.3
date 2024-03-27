02bf69c6e240d0d6b268bd7465d75abd7e337d12
DopplerXD <1509209607@qq.com>
2024-03-27 20:56:47 +0800

    Initial commit

---
+++ src/main/java/site/dopplerxd/TutorialModClient.java
@@ -0,0 +1,17 @@
+package site.dopplerxd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @Author: Doppler
+ * @Project: {tutorial-mod-template-1.19.3}
+ * @Package: {site.dopplerxd}
+ * @Date: {2024/3/27}
+ * @Time: {20:49}
+ */
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
